c3fa7b7fb38950e0f0da8794b2d38b12a633b897
Guangwei Li <liguwe@qq.com>
2022-12-18 17:10:35 +0800

    feat: 到此为止吧！后面就直接看代码就好了

--- 60.js
+++
@@ -1,9 +0,0 @@
-function myNew(fn,...args) {
-
-    const obj = Object.create(fn.prototype);
-
-    const res = fn.apply(obj,args);
-
-    return res instanceof Object ? res : obj;
-
-}
--- 95-4.js
+++
@@ -1,8 +0,0 @@
-/*************************************************
- *  :::: 优化吸顶
- *  1、ios不能实时响应，页面滚动停止之后才会出现具体效果
- *      =>解决方案：IOS 使用 position:sticky，Android 使用滚动监听 getBoundingClientRect().top 的值。
- *  2、使用window.requestAnimationFrame()来优化 => 牺牲平滑度
- *  3、window.requestIdleCallback()来优化  => 牺牲平滑度
- *  3、节流  => 也会牺牲平滑度
- ************************************************/
