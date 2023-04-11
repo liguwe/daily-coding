8ebca5d5ba6fb7bf74d3fdc275e2e2dc18324af7
liguangwei007 <liguwe@qq.com>
2023-04-11 08:36:44 +0800

    feat: update

--- code-for-vue-3-book-master/course4-响应系统/1. 响应系统的作用与实现/1.js
+++ code-for-vue-3-book-master/course4-响应系统/1. 响应系统的作用与实现/1.js
@@ -1,18 +1,72 @@
 const data = {foo: 1};
 const obj = new Proxy(data, {});
-
 effect(
     () => {
-        console.log(obj.foo);
+        console.log(obj.foo); // ::::立即执行
     }
 );
 
-obj.foo++;
-obj.foo++;
-obj.foo++;
-obj.foo++;
+// ::::添加 options.lazy = true, 使得 effect 不会立即执行 ?
+effect(
+    () => {
+        console.log(obj.foo); // ::::不立即执行
+    },
+    // ::::options.lazy = true 时不立即执行
+    {lazy: true}
+);
 
-// ::::顺序就可以变成 1 2 3 4 5 6 ...
+
+let activeEffect
+const effectStack = []
+function effect(fn, options = {}) {
+    const effectFn = () => {
+        cleanup(effectFn)
+        activeEffect = effectFn
+        effectStack.push(effectFn)
+        effectStack.pop()
+        activeEffect = effectStack[effectStack.length - 1]
+    }
+    effectFn.options = options
+    effectFn.deps = []
+    // :::: 执行副作用函数
+    if (!options.lazy) {
+        effectFn()
+    }
+    // ::::options.lazy = true , 不立即执行，返回 effectFn
+    return effectFn
+}
+
+const fn1 = effect(
+    () => {
+        console.log(obj.foo);
+    },
+    {lazy: true}
+);
+
+fn1();// ::::手动执行
 
 
+
+// ::::有没有一定计算属性的影子？？
+const sumFn = effect(
+    () => {
+        return obj.a + obj.b;
+    },
+    {lazy: true}
+);
+const sum = sumFn();
+
+effect(() => {
+    console.log(obj.foo);
+})
+
+function watch(source,cb) {
+    effect(() => {
+        console.log(source.foo);
+    },{
+        scheduler: () => {
+            cb()
+        }
+    })
+}
 
